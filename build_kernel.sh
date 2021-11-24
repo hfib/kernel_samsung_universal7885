86fda370dc20da93a4c3e2125dee6803591c1809
Hf1b <itzdaniel1337@gmail.com>
2021-11-24 21:00:55 +0500

    updated build script for my purposes

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,5 @@ export PLATFORM_VERSION=10
 export ANDROID_MAJOR_VERSION=q
 export ARCH=arm64
 
-make ARCH=arm64 exynos7885-a30s_oneui_defconfig
+make ARCH=arm64 exynos7885-a30s_oneui_external_defconfig
 make ARCH=arm64 -j8
